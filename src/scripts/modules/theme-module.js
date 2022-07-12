bcb509de9d2c3f4a96330594a5a539e211653599
Leo Paul Mamangun <mamangunleopaul01@gmail.com>
2022-07-12 16:01:01 +0800

    ..

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -14,45 +14,108 @@ AppName.Modules.ThemeModule = (function () {
 
   var _megaMenu = () => {
 
+    function checkWidth() {
 
-    $(".with-inner-list").each(function(){
+      $vWidth = $(window).width();
 
-        $(this).hover(function() {
+      if ($vWidth > 991) {
+        $(".with-inner-list").each(function(){
 
-          var label = $(this).attr("data-label");
-
-          $('.with-inner-list').removeClass('active');
-          $(this).addClass('active');
-
-
-          $('.drop-menu-list-inner').find('#' + label).siblings().hide();
-          $('.drop-menu-list-inner').find('#' + label).show();
-
+          $(this).hover(function() {
+  
+            var label = $(this).attr("data-label");
+  
+            $('.with-inner-list').removeClass('active');
+            $(this).addClass('active');
+  
+  
+            $('.drop-menu-list-inner').find('#' + label).siblings().hide();
+            $('.drop-menu-list-inner').find('#' + label).show();
+  
+          });
+  
         });
+      } else {
+        $('.next-stage').each(function(){
+          $(this).click(function() {
+            $('.drop-menu').removeClass('show');
+            $(this).siblings('.drop-menu').addClass('show');      
+          });
+        });
+  
+        $('.next-stage-inner').each(function(){
+  
+          $(this).click(function() {
+  
+            var label = $(this).siblings('.with-inner-list').attr("data-label");
+  
+            $('.drop-menu-list-inner').addClass('show');
+  
+            $('.drop-menu-list-inner').find('#' + label).siblings().removeClass('show');
+            $('.drop-menu-list-inner').find('#' + label).addClass('show');
+  
+          });
+  
+        });
+  
+        $('.navbar-toggler').click(function(){
+  
+          $('.drop-menu').removeClass('show');
+          $('.drop-menu-list-inner').removeClass('show');
+          $('.inner-list').removeClass('show');
+        });
+  
+        $('.drop-menu-list .back').click(function(){
+          $('.drop-menu').removeClass('show');
+        });
+  
+        $('.drop-menu-list-inner .back').click(function(){
+          $('.drop-menu-list-inner').removeClass('show');
+          // $('.inner-list').removeClass('show');
+        });
+      }
 
+    }
+    
+    checkWidth();
+
+    function stickyNav() {
+      var scroll = $(window).scrollTop();
+      var $vWidth = $(window).width();
+
+      if ($vWidth > 991) {
+
+        if (scroll >= 25) {
+          $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+
+      } else {
+
+        if (scroll >= 1) {
+          $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+
+      }
+
+    }
+    stickyNav();
+
+
+    $(window).scroll(function() {    
+      stickyNav();
     });
 
-
-    $('.next-stage').each(function(){
-      $(this).click(function() {
-        $('.drop-menu').removeClass('show');
-        $(this).siblings('.drop-menu').addClass('show');      
-      });
-    });
-
-    $('.next-stage-inner').each(function(){
-
-      $(this).click(function() {
-
-        var label = $(this).siblings('.with-inner-list').attr("data-label");
-
-        $('.drop-menu-list-inner').find('#' + label).addClass('show');
-
-      });
-
+    $(window).on("resize", function(){
+      stickyNav();
+      checkWidth();
     });
 
   }
+
 
   /////////////////////
   // Public Methods //
