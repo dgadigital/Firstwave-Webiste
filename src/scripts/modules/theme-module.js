1b61d73eca394d06d37abae762a538bf19c74906
christianfloress <christian.flores@websiteblue.com>
2022-07-13 01:19:08 +0800

    Investor Page: Subscribe Section, Hero Section, and Tabs Resources Section (this section still in-progress but the only remaining are the contents per tab).

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -7,76 +7,70 @@ AppName.Modules.ThemeModule = (function () {
   ////////////////////
   const _privateMethod = () => {
     // private stuff
-
-
   };
 
-
   var _megaMenu = () => {
-
     function checkWidth() {
-
       $vWidth = $(window).width();
 
       if ($vWidth > 991) {
-        $(".with-inner-list").each(function(){
-
-          $(this).hover(function() {
-  
+        $(".with-inner-list").each(function () {
+          $(this).hover(function () {
             var label = $(this).attr("data-label");
-  
-            $('.with-inner-list').removeClass('active');
-            $(this).addClass('active');
-  
-  
-            $('.drop-menu-list-inner').find('#' + label).siblings().hide();
-            $('.drop-menu-list-inner').find('#' + label).show();
-  
+
+            $(".with-inner-list").removeClass("active");
+            $(this).addClass("active");
+
+            $(".drop-menu-list-inner")
+              .find("#" + label)
+              .siblings()
+              .hide();
+            $(".drop-menu-list-inner")
+              .find("#" + label)
+              .show();
           });
-  
         });
       } else {
-        $('.next-stage').each(function(){
-          $(this).click(function() {
-            $('.drop-menu').removeClass('show');
-            $(this).siblings('.drop-menu').addClass('show');      
+        $(".next-stage").each(function () {
+          $(this).click(function () {
+            $(".drop-menu").removeClass("show");
+            $(this).siblings(".drop-menu").addClass("show");
           });
         });
-  
-        $('.next-stage-inner').each(function(){
-  
-          $(this).click(function() {
-  
-            var label = $(this).siblings('.with-inner-list').attr("data-label");
-  
-            $('.drop-menu-list-inner').addClass('show');
-  
-            $('.drop-menu-list-inner').find('#' + label).siblings().removeClass('show');
-            $('.drop-menu-list-inner').find('#' + label).addClass('show');
-  
+
+        $(".next-stage-inner").each(function () {
+          $(this).click(function () {
+            var label = $(this).siblings(".with-inner-list").attr("data-label");
+
+            $(".drop-menu-list-inner").addClass("show");
+
+            $(".drop-menu-list-inner")
+              .find("#" + label)
+              .siblings()
+              .removeClass("show");
+            $(".drop-menu-list-inner")
+              .find("#" + label)
+              .addClass("show");
           });
-  
         });
-  
-        $('.navbar-toggler').click(function(){
-  
-          $('.drop-menu').removeClass('show');
-          $('.drop-menu-list-inner').removeClass('show');
-          $('.inner-list').removeClass('show');
+
+        $(".navbar-toggler").click(function () {
+          $(".drop-menu").removeClass("show");
+          $(".drop-menu-list-inner").removeClass("show");
+          $(".inner-list").removeClass("show");
         });
-  
-        $('.drop-menu-list .back').click(function(){
-          $('.drop-menu').removeClass('show');
+
+        $(".drop-menu-list .back").click(function () {
+          $(".drop-menu").removeClass("show");
         });
-  
-        $('.drop-menu-list-inner .back').click(function(){
-          $('.drop-menu-list-inner').removeClass('show');
+
+        $(".drop-menu-list-inner .back").click(function () {
+          $(".drop-menu-list-inner").removeClass("show");
           // $('.inner-list').removeClass('show');
         });
       }
-
     }
-    
+
     checkWidth();
 
     function stickyNav() {
@@ -84,45 +78,33 @@ AppName.Modules.ThemeModule = (function () {
       var $vWidth = $(window).width();
 
       if ($vWidth > 991) {
-
         if (scroll >= 25) {
           $(".navbar").addClass("sticky");
         } else {
-            $(".navbar").removeClass("sticky");
+          $(".navbar").removeClass("sticky");
         }
-
       } else {
-
         if (scroll >= 1) {
           $(".navbar").addClass("sticky");
         } else {
-            $(".navbar").removeClass("sticky");
+          $(".navbar").removeClass("sticky");
         }
-
       }
-
     }
     stickyNav();
 
-
-    $(window).scroll(function() {    
+    $(window).scroll(function () {
       stickyNav();
     });
 
-    $(window).on("resize", function(){
+    $(window).on("resize", function () {
       stickyNav();
       checkWidth();
     });
-
-  }
-
-
-  
-
+  };
 
   var _slickSliders = () => {
-
-    $('.blog-post-slider').slick({
+    $(".blog-post-slider").slick({
       dots: true,
       infinite: true,
       speed: 300,
@@ -132,61 +114,72 @@ AppName.Modules.ThemeModule = (function () {
         {
           breakpoint: 767,
           settings: {
-            slidesToShow: 1
-          }
-        }
-      ]
+            slidesToShow: 1,
+          },
+        },
+      ],
     });
-
-  }
-  var _blogpostElements = () => {
-
-    function setmaxHeight(){
-      $('.blog-post-slider').each(function(){    
-        var blogTitle = 0;  
-        var blogExcerpt = 0;
-
-        $('.blog-post-slider article .post-title').css( {
-          'min-height': '0'
-        });
-
-        $('.blog-post-slider article .post-excerpt').css( {
-            'min-height': '0'
-        });  
-  
-        $('article .post-title', this).each(function(){
-          if($(this).height() > blogTitle) {
-            blogTitle = $(this).height(); 
-          }
-        
-        });  
-  
-        $('article .post-excerpt', this).each(function(){
-          if($(this).height() > blogExcerpt) {
-            blogExcerpt = $(this).height(); 
-          }
-        
-        });  
-
-        
-        $('.blog-post-slider article .post-title').css( {
-            'min-height': blogTitle
-        });
-
-        $('.blog-post-slider article .post-excerpt').css( {
-            'min-height': blogExcerpt
-        });     
-
-      });   
-    }
-      
-    setmaxHeight();
-
-    window.onresize = function(event) {
-      setmaxHeight();
   };
 
-  }
+  var _blogpostElements = () => {
+    function setmaxHeight() {
+      $(".blog-post-slider").each(function () {
+        var blogTitle = 0;
+        var blogExcerpt = 0;
+
+        $(".blog-post-slider article .post-title").css({
+          "min-height": "0",
+        });
+
+        $(".blog-post-slider article .post-excerpt").css({
+          "min-height": "0",
+        });
+
+        $("article .post-title", this).each(function () {
+          if ($(this).height() > blogTitle) {
+            blogTitle = $(this).height();
+          }
+        });
+
+        $("article .post-excerpt", this).each(function () {
+          if ($(this).height() > blogExcerpt) {
+            blogExcerpt = $(this).height();
+          }
+        });
+
+        $(".blog-post-slider article .post-title").css({
+          "min-height": blogTitle,
+        });
+
+        $(".blog-post-slider article .post-excerpt").css({
+          "min-height": blogExcerpt,
+        });
+      });
+    }
+
+    setmaxHeight();
+
+    window.onresize = function (event) {
+      setmaxHeight();
+    };
+  };
+
+  var _tabsResources = () => {
+    var firstTab = $(".section-tabs-resources .nav-pills li:first").html();
+    $(".section-tabs-resources .tabs-mobile li").html(firstTab);
+
+    $(".section-tabs-resources .tabs-mobile").click(function () {
+      $(this).parent().toggleClass("active");
+    });
+
+    $(".section-tabs-resources .nav-pills a").click(function () {
+      var currentele = $(this).html();
+      $(".section-tabs-resources .tabs-mobile a").html(currentele);
+      $(this)
+        .parents(".section-tabs-resources .tabs-holder")
+        .removeClass("active");
+    });
+  };
 
   /////////////////////
   // Public Methods //
@@ -194,8 +187,9 @@ AppName.Modules.ThemeModule = (function () {
   const init = function () {
     _privateMethod();
     _megaMenu();
-    _slickSliders();
+    // _slickSliders();
     _blogpostElements();
+    _tabsResources();
     // _footerMenu();
   };
 
